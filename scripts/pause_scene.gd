e520af35ceb33a59ea1194162455a7ec2e3987e8
Sokovikova Diana <dianasokovikova171@gmail.com>
2025-10-24 15:06:08 +0700

    Доплнения для удобства (прочитайте README)

--- scripts/pause_scene.gd
+++ scripts/pause_scene.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+# просто открывается поверх геймплея
+
 func _on_resume_button_pressed() -> void:
 	pass # Replace with function body.
 
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+# для начала будет достаточно реализовать правильно движение + зону, в которой игроку будет 
+# доступно взаимодействие с предметами. 
+
+
 signal player_initiated_dialogue() #диалог с нпс или взаимодействие с предметом
 signal player_initiated_cutscene()
 signal player_initiated_combat()
