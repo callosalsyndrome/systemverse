14ab3a3f790f229ce35e6d982c758bffb209121d
Sokovikova Diana <dianasokovikova171@gmail.com>
2025-11-22 19:14:19 +0700

    добавила некоторые анимации, удалила лишние и улучшила код игрока

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,7 +9,7 @@ signal player_initiated_cutscene()
 signal player_initiated_combat()
 signal checkpoint_reached()
 
-@export var speed: int = 200
+@export var speed: int = 150
 var last_direction = ""
 var is_moving = false
 
@@ -17,96 +17,118 @@ func _physics_process(_delta):
 	var input_direction = Vector2.ZERO
 	is_moving = false
 	
-	#право
-	if Input.is_action_pressed("move_right"):
-		input_direction.x += 1
-		$AnimatedSprite2D.play("horisontal_right")
-		$AnimatedSprite2D.flip_h = false
-		last_direction = "right"
-		is_moving = true
-	
-	#лево
-	if Input.is_action_pressed("move_left"):
-		input_direction.x -= 1
-		$AnimatedSprite2D.play("horisontal_right")
-		$AnimatedSprite2D.flip_h = true
-		last_direction = "left"
-		is_moving = true
-	
-	#низ
-	if Input.is_action_pressed("move_down"):
-		input_direction.y += 1
-		$AnimatedSprite2D.play("vertical_down")
-		last_direction = "down"
-		is_moving = true
-	
-	#верх
-	if Input.is_action_pressed("move_up"):
-		input_direction.y -= 1
-		$AnimatedSprite2D.play("vertical_up")
-		last_direction = "up"
-		is_moving = true
-	
-	#низ право
+	# Сначала проверяем диагональные движения
 	if Input.is_action_pressed("move_down") and Input.is_action_pressed("move_right"):
-		$AnimatedSprite2D.animation = "diagonal_down"
+		input_direction.x += 1
+		input_direction.y += 1
 		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.animation = "diagonal_down"
 		last_direction = "down_right"
 		is_moving = true
 	
-	#низ лево
-	if Input.is_action_pressed("move_down") and Input.is_action_pressed("move_left"):
-		$AnimatedSprite2D.animation = "diagonal_down"
+	elif Input.is_action_pressed("move_down") and Input.is_action_pressed("move_left"):
+		input_direction.x -= 1
+		input_direction.y += 1
 		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.animation = "diagonal_down"
 		last_direction = "down_left"
 		is_moving = true
 	
-	#верх право
-	if Input.is_action_pressed("move_up") and Input.is_action_pressed("move_right"):
-		$AnimatedSprite2D.animation = "diagonal_up"
+	elif Input.is_action_pressed("move_up") and Input.is_action_pressed("move_right"):
+		input_direction.x += 1
+		input_direction.y -= 1
 		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.animation = "diagonal_up"
 		last_direction = "up_right"
 		is_moving = true
 	
-	#верх лево
-	if Input.is_action_pressed("move_up") and Input.is_action_pressed("move_left"):
-		$AnimatedSprite2D.animation = "diagonal_up"
+	elif Input.is_action_pressed("move_up") and Input.is_action_pressed("move_left"):
+		input_direction.x -= 1
+		input_direction.y -= 1
 		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.animation = "diagonal_up"
 		last_direction = "up_left"
 		is_moving = true
 	
+	# Затем проверяем простые направления
+	else:
+		#право
+		if Input.is_action_pressed("move_right"):
+			input_direction.x += 1
+			$AnimatedSprite2D.play("horisontal")
+			$AnimatedSprite2D.flip_h = false
+			last_direction = "right"
+			is_moving = true
+		
+		#лево
+		if Input.is_action_pressed("move_left"):
+			input_direction.x -= 1
+			$AnimatedSprite2D.flip_h = true
+			$AnimatedSprite2D.play("horisontal")
+			last_direction = "left"
+			is_moving = true
+		
+		#низ
+		if Input.is_action_pressed("move_down"):
+			input_direction.y += 1
+			$AnimatedSprite2D.play("vertical_down")
+			last_direction = "down"
+			is_moving = true
+		
+		#верх
+		if Input.is_action_pressed("move_up"):
+			input_direction.y -= 1
+			$AnimatedSprite2D.play("vertical_up")
+			last_direction = "up"
+			is_moving = true
+	
+	# Нормализуем вектор направления для диагонального движения
+	if input_direction.length() > 0:
+		input_direction = input_direction.normalized()
+	
+	# Двигаем персонажа
+	position += input_direction * speed * _delta
+	
 	#попытка в остановку анимации
-	if (is_moving == false):
+	if not is_moving:
 		match last_direction:
 			"down":
-				$AnimatedSprite2D.animation = "vertical_down_stand"
+				$AnimatedSprite2D.animation = "vertical_down"
+				$AnimatedSprite2D.stop()
+				$AnimatedSprite2D.frame = 1
 			"up":
-				$AnimatedSprite2D.animation = "vertical_up_stand"
+				$AnimatedSprite2D.animation = "vertical_up"
+				$AnimatedSprite2D.stop()
+				$AnimatedSprite2D.frame = 4
 			"right":
-				$AnimatedSprite2D.animation = "horisontal_right_stand"
 				$AnimatedSprite2D.flip_h = false
+				$AnimatedSprite2D.animation = "horisontal"
+				$AnimatedSprite2D.stop()
+				$AnimatedSprite2D.frame = 7
 			"left":
-				$AnimatedSprite2D.animation = "horisontal_right_stand"
 				$AnimatedSprite2D.flip_h = true
-			#"down_right":
-				#$AnimatedSprite2D.animation = ""
-				#$AnimatedSprite2D.flip_h = false
-			#"down_left":
-				#$AnimatedSprite2D.animation = ""
-				#$AnimatedSprite2D.flip_h = true
-			#"up_right":
-				#$AnimatedSprite2D.animation = ""
-				#$AnimatedSprite2D.flip_h = false
-			#"up_left":
-				#$AnimatedSprite2D.animation = ""
-				#$AnimatedSprite2D.flip_h = true
-	
-	#нормализация для диагоналей, применение скорости
-	if input_direction != Vector2.ZERO:
-		input_direction = input_direction.normalized()
-		velocity = input_direction * speed
-	else:
-		velocity = Vector2.ZERO
-	move_and_slide()
+				$AnimatedSprite2D.animation = "horisontal"
+				$AnimatedSprite2D.stop()
+				$AnimatedSprite2D.frame = 7
+			"up_right":
+				$AnimatedSprite2D.flip_h = false
+				$AnimatedSprite2D.animation = "diagonal_up"
+				$AnimatedSprite2D.stop()
+				$AnimatedSprite2D.frame = 0
+			"up_left":
+				$AnimatedSprite2D.flip_h = true
+				$AnimatedSprite2D.animation = "diagonal_up"
+				$AnimatedSprite2D.stop()
+				$AnimatedSprite2D.frame = 0
+			"down_right":
+				$AnimatedSprite2D.flip_h = false
+				$AnimatedSprite2D.animation = "diagonal_down"
+				$AnimatedSprite2D.stop()
+				$AnimatedSprite2D.frame = 1
+			"down_left":
+				$AnimatedSprite2D.flip_h = true
+				$AnimatedSprite2D.animation = "diagonal_down"
+				$AnimatedSprite2D.stop()
+				$AnimatedSprite2D.frame = 1
 	
 	
