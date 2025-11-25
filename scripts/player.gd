7f3e36d86540653de5ad685c817dd1eb9c3d0704
kzn2220 <kuzn.a2220@gmail.com>
2025-11-25 18:06:43 +0700

    пофиксила ту фигню с переходом, добавила ускорение на шифт

--- scripts/player.gd
+++ scripts/player.gd
@@ -86,6 +86,11 @@ func _physics_process(_delta):
 	if input_direction.length() > 0:
 		input_direction = input_direction.normalized()
 	
+	if Input.is_action_pressed("speed"):
+		speed = 250
+	else:
+		speed = 150
+	
 	# Двигаем персонажа
 	#position += input_direction * speed * _delta #ЛОМАЕТ ВСЮ ФИЗИКУ СТОЛКНОВЕНИЙ
 	
