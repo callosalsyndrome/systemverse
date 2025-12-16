203dcd088cb0a966c58f0e12901d89085464d7dd
Sokovikova Diana <dianasokovikova171@gmail.com>
2025-12-16 19:40:05 +0700

    почистила лишние скрипты и удалила ненужные файлы

--- scripts/make_later/DialogueManager.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-signal dialogue_started()
-signal dialogue_ended()
-signal cutscene_started()
-signal cutscene_ended()
--- scripts/save_slot.gd
+++ scripts/save_slot.gd
@@ -1,7 +1,7 @@
 extends Button
 
 # Первая кнопка
-
+# не удалять
 
 func _on_button_pressed() -> void:
 	pass # Replace with function body.
